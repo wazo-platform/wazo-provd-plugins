81f06807cfc904bd64992bec6ea2cce20e7525f5
sylvain boily <sboily@proformatique.com>
2012-12-13 09:32:19 -0500

    missing file

--- plugins/xivo-cisco-spa/spa8000-6.1.11/entry.py
+++ plugins/xivo-cisco-spa/spa8000-6.1.11/entry.py
@@ -6,7 +6,7 @@ common_globals = {}
 execfile_('common.py', common_globals)
 
 
-MODEL_VERSION = {u'SPA8000': u'6.1.3'}
+MODEL_VERSION = {u'SPA8000': u'6.1.11'}
 
 
 class CiscoPlugin(common_globals['BaseCiscoPlugin']):
