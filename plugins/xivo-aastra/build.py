579de40e4470f51921407ddfaec0a4bbfe49580c
Etienne Lessard <elessard@avencall.com>
2013-04-01 13:06:52 -0400

    remove plugin xivo-aastra-3.2.2.1136
    Superseded by xivo-aastra-3.2.2.2112.

--- plugins/xivo-aastra/build.py
+++ plugins/xivo-aastra/build.py
@@ -20,16 +20,6 @@ def build_2_6_0_2019(path):
                 '2.6.0.2019/', path])
 
 
-@target('3.2.2.1136', 'xivo-aastra-3.2.2.1136')
-def build_3_2_2_1136(path):
-    check_call(['rsync', '-rlp', '--exclude', '.*',
-                '--exclude', '/templates/6751i.tpl',
-                'common/', path])
-
-    check_call(['rsync', '-rlp', '--exclude', '.*',
-                '3.2.2.1136/', path])
-
-
 @target('3.2.2.2112', 'xivo-aastra-3.2.2.2112')
 def build_3_2_2_2112(path):
     check_call(['rsync', '-rlp', '--exclude', '.*',
