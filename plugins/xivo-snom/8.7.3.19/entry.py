b38b116e3680927bf5158f47b1b9b701c0ac0864
Etienne Lessard <elessard@avencall.com>
2013-10-17 08:10:02 -0400

    minor reformating

--- plugins/xivo-snom/8.7.3.19/entry.py
+++ plugins/xivo-snom/8.7.3.19/entry.py
@@ -3,8 +3,20 @@
 common_globals = {}
 execfile_('common.py', common_globals)
 
-
-MODELS = [u'300', u'320', u'360', u'370', u'710', u'720', u'760', u'820', u'821', u'870', u'MP', u'PA1']
+MODELS = [
+    u'300',
+    u'320',
+    u'360',
+    u'370',
+    u'710',
+    u'720',
+    u'760',
+    u'820',
+    u'821',
+    u'870',
+    u'MP',
+    u'PA1',
+]
 VERSION = u'8.7.3.19'
 
 
--- plugins/xivo-snom/8.7.4.8/entry.py
+++ plugins/xivo-snom/8.7.4.8/entry.py
@@ -3,8 +3,19 @@
 common_globals = {}
 execfile_('common.py', common_globals)
 
-
-MODELS = [u'300', u'320', u'370', u'710', u'720', u'760', u'820', u'821', u'870', u'MP', u'PA1']
+MODELS = [
+    u'300',
+    u'320',
+    u'370',
+    u'710',
+    u'720',
+    u'760',
+    u'820',
+    u'821',
+    u'870',
+    u'MP',
+    u'PA1',
+]
 VERSION = u'8.7.4.8'
 
 
