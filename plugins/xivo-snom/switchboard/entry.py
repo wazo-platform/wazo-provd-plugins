38a8714c0302ad37c31aef77bf13e7b3bbbfdeb9
Etienne Lessard <elessard@avencall.com>
2013-10-22 10:24:44 -0400

    disable automatic plugin association for xivo-snom-switchboard

--- plugins/xivo-snom/switchboard/entry.py
+++ plugins/xivo-snom/switchboard/entry.py
@@ -6,12 +6,9 @@ execfile_('common.py', common_globals)
 MODELS = [
     u'720',
 ]
-VERSION = u'8.7.3.19'
 
 
 class SnomPlugin(common_globals['BaseSnomPlugin']):
     IS_PLUGIN = True
 
     _MODELS = MODELS
-
-    pg_associator = common_globals['BaseSnomPgAssociator'](MODELS, VERSION)
