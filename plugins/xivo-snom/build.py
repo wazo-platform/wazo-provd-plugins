bcde30b032d22c318f56dea9706ea5f57c45e4a0
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2014-03-04 07:32:50 -0500

    remove plugin xivo-snom 8.7.3.25

--- plugins/xivo-snom/build.py
+++ plugins/xivo-snom/build.py
@@ -23,51 +23,6 @@ import os.path
 from subprocess import check_call
 
 
-@target('8.7.3.25', 'xivo-snom-8.7.3.25')
-def build_8_7_3_25(path):
-    MODELS = [('300', 'f'),
-              ('320', 'f'),
-              ('360', 'f'),
-              ('370', 'f'),
-              ('710', 'r'),
-              ('720', 'r'),
-              ('760', 'r'),
-              ('820', 'r'),
-              ('821', 'r'),
-              ('870', 'r'),
-              ('MP', 'r'),
-              ('PA1', 'f')]
-
-    check_call(['rsync', '-rlp', '--exclude', '.*',
-                '--exclude', '*.btpl',
-                'common/', path])
-
-    for model, fw_suffix in MODELS:
-        # generate snom<model>-firmware.xml.tpl from snom-model-firmware.xml.tpl.btpl
-        model_tpl = os.path.join(path, 'templates', 'common', 'snom%s-firmware.xml.tpl' % model)
-        sed_script = 's/#FW_FILENAME#/snom%s-8.7.3.25-SIP-%s.bin/' % (model, fw_suffix)
-        with open(model_tpl, 'wb') as f:
-            check_call(['sed', sed_script, 'common/templates/common/snom-model-firmware.xml.tpl.btpl'],
-                       stdout=f)
-
-        # generate snom<model>.htm.tpl from snom-model.htm.tpl.mtpl
-        model_tpl = os.path.join(path, 'templates', 'common', 'snom%s.htm.tpl' % model)
-        sed_script = 's/#MODEL#/%s/' % model
-        with open(model_tpl, 'wb') as f:
-            check_call(['sed', sed_script, 'common/templates/common/snom-model.htm.tpl.btpl'],
-                       stdout=f)
-
-        # generate snom<model>.xml.tpl from snom-model.xml.mtpl
-        model_tpl = os.path.join(path, 'templates', 'common', 'snom%s.xml.tpl' % model)
-        sed_script = 's/#MODEL#/%s/' % model
-        with open(model_tpl, 'wb') as f:
-            check_call(['sed', sed_script, 'common/templates/common/snom-model.xml.tpl.btpl'],
-                       stdout=f)
-
-    check_call(['rsync', '-rlp', '--exclude', '.*',
-                '8.7.3.25/', path])
-
-
 @target('8.7.3.25.5', 'xivo-snom-8.7.3.25.5')
 def build_8_7_3_25_5(path):
     MODELS = [('300', 'f'),
