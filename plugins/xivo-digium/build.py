41d4105043622f0df9fdf41eda6734dbe5f31996
Etienne Lessard <elessard@avencall.com>
2014-06-27 09:16:01 -0400

    delete old xivo-digium-1.3.3.0 plugin

--- plugins/xivo-digium/build.py
+++ plugins/xivo-digium/build.py
@@ -18,14 +18,6 @@
 from subprocess import check_call
 
 
-@target('1.3.3.0', 'xivo-digium-1.3.3.0')
-def build_1_3_3_0(path):
-    check_call(['rsync', '-rlp', '--exclude', '.*',
-                'common/', path])
-
-    check_call(['rsync', '-rlp', '--exclude', '.*',
-                '1.3.3.0/', path])
-
 @target('1.4.0.0', 'xivo-digium-1.4.0.0')
 def build_1_4_0_0(path):
     check_call(['rsync', '-rlp', '--exclude', '.*',
