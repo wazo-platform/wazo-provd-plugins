2ba828a29ea5c0030f6264cd30548c0bf15de41a
Sylvain Boily <sboily@avencall.com>
2015-02-11 17:41:23 +0100

    added 4.0.0 aastra firmware for 68XX series

---
+++ plugins/xivo-aastra/4.0.0-SP1/entry.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import logging
+
+common = {}
+execfile_('common.py', common)
+
+logger = logging.getLogger('plugin.xivo-aastra')
+
+
+MODEL_VERSIONS = {
+    u'6863i': u'4.0.0.1096',
+    u'6865i': u'4.0.0.1096',
+    u'6867i': u'4.0.0.1096',
+    u'6869i': u'4.0.0.1096',
+}
+
+
+class AastraPlugin(common['BaseAastraPlugin']):
+    IS_PLUGIN = True
+
+    pg_associator = common['BaseAastraPgAssociator'](MODEL_VERSIONS)
+
+    def _add_parking(self, raw_config):
+        # hack to set the per line parking config if a park function key is used
+        parking = None
+        is_parking_set = False
+        for funckey_no, funckey_dict in raw_config[u'funckeys'].iteritems():
+            if funckey_dict[u'type'] == u'park':
+                if is_parking_set:
+                    cur_parking = funckey_dict[u'value']
+                    if cur_parking != parking:
+                        logger.warning('Ignoring park value %s for function key %s: using %s',
+                                       cur_parking, funckey_no, parking)
+                else:
+                    parking = funckey_dict[u'value']
+                    is_parking_set = True
+                    self._do_add_parking(raw_config, parking)
+
+    def _do_add_parking(self, raw_config, parking):
+        raw_config[u'XX_parking'] = u'\n'.join(u'sip line%s park pickup config: %s;%s;asterisk' %
+                                               (line_no, parking, parking)
+                                               for line_no in raw_config[u'sip_lines'])
