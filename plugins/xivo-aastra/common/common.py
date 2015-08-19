026cbf0e5248442a0536a4e08b186d7cdbf06e8a
Etienne Lessard <elessard@avencall.com>
2015-08-19 08:20:35 -0400

    xivo-aastra: remove duplicate code in entry.py
    This was used when there was a xivo-aastra-2.6.x plugin.

--- plugins/xivo-aastra/common/common.py
+++ plugins/xivo-aastra/common/common.py
@@ -374,8 +374,25 @@ class BaseAastraPlugin(StandardPlugin):
                                                                     self._TRUSTED_ROOT_CERTS_SUFFIX)
 
     def _add_parking(self, raw_config):
-        # to be optionally overridden in derived class
-        pass
+        # hack to set the per line parking config if a park function key is used
+        parking = None
+        is_parking_set = False
+        for funckey_no, funckey_dict in raw_config[u'funckeys'].iteritems():
+            if funckey_dict[u'type'] == u'park':
+                if is_parking_set:
+                    cur_parking = funckey_dict[u'value']
+                    if cur_parking != parking:
+                        logger.warning('Ignoring park value %s for function key %s: using %s',
+                                       cur_parking, funckey_no, parking)
+                else:
+                    parking = funckey_dict[u'value']
+                    is_parking_set = True
+                    self._do_add_parking(raw_config, parking)
+
+    def _do_add_parking(self, raw_config, parking):
+        raw_config[u'XX_parking'] = u'\n'.join(u'sip line%s park pickup config: %s;%s;asterisk' %
+                                               (line_no, parking, parking)
+                                               for line_no in raw_config[u'sip_lines'])
 
     def _dev_specific_filename(self, device):
         # Return the device specific filename (not pathname) of device
