7ff596d4555ce584f576022acaa08474756658a5
Sylvain Boily <sylvainboilydroid@gmail.com>
2017-02-07 20:50:31 -0500

    add w52p, w56p and w56h support with firmware v80

--- plugins/xivo-yealink/v80/entry.py
+++ plugins/xivo-yealink/v80/entry.py
@@ -31,6 +31,7 @@ MODEL_VERSIONS = {
     u'T46G': u'28.80.0.95',
     u'T48G': u'35.80.0.95',
     u'T49G': u'51.80.0.75',
+    u'W52P': u'25.80.0.15',
 }
 COMMON_FILES = [
     ('y000000000028.cfg', u'T46-28.80.0.95.rom', 'model.tpl'),
@@ -46,6 +47,10 @@ COMMON_FILES = [
     ('y000000000054.cfg', u'T40-54.80.0.95.rom', 'model.tpl'),
 ]
 
+COMMON_FILES_DECT = [
+    ('y000000000025.cfg', u'Base for W52P&W56P-25.80.0.15.rom', u'W56H-61.80.0.15.rom', 'W52P.tpl'),
+]
+
 
 class YealinkPlugin(common_globals['BaseYealinkPlugin']):
     IS_PLUGIN = True
@@ -55,3 +60,12 @@ class YealinkPlugin(common_globals['BaseYealinkPlugin']):
     # Yealink plugin specific stuff
 
     _COMMON_FILES = COMMON_FILES
+
+    def configure_common(self, raw_config):
+        super(YealinkPlugin, self).configure_common(raw_config)
+        for filename, fw_filename, fw_handset_filename, tpl_filename in COMMON_FILES_DECT:
+            tpl = self._tpl_helper.get_template('common/%s' % tpl_filename)
+            dst = os.path.join(self._tftpboot_dir, filename)
+            raw_config[u'XX_fw_filename'] = fw_filename
+            raw_config[u'XX_fw_handset_filename'] = fw_handset_filename
+            self._tpl_helper.dump(tpl, raw_config, dst, self._ENCODING)
