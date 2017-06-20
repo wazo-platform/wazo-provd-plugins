827d8e0fc02beaf699e5c5be6d2155b18d4fdeac
Pascal Cadotte Michaud <pcm@wazo.community>
2017-06-20 13:36:19 -0400

    copy xivo-zenitel from xivo-provd-plugins-addons

---
+++ plugins/xivo-zenitel/build.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013-2014 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+# Depends on the following external programs:
+#  -rsync
+
+from subprocess import check_call
+
+
+@target('01.11.3.2', 'xivo-zenitel-01.11.3.2')
+def build_01_11_3_2(path):
+    check_call(['rsync', '-rlp', '--exclude', '.*',
+                'common/', path])
+    
+    check_call(['rsync', '-rlp', '--exclude', '.*',
+                '01.11.3.2/', path])
