738b5823a9ad579f468cd6015d42bb5cf1f85567
Sylvain Boily <sboily@avencall.com>
2017-10-11 14:36:02 +0200

    add template

---
+++ plugins/wazo-htek/build.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2017 Wazo Authors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+# Depends on the following external programs:
+#  -rsync
+
+from subprocess import check_call
+
+
+@target('2.0.4.2.19', 'wazo-htek-2.0.4.2.19')
+def build_2_0_4_2_19(path):
+    check_call(['rsync', '-rlp', '--exclude', '.*',
+                'common/', path])
+
+    check_call(['rsync', '-rlp', '--exclude', '.*',
+                '2.0.4.2.19/', path])
