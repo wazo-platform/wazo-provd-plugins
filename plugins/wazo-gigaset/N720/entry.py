9e0e8b000882b98e6fdbe311ccfc57e5798a9077
Alexandre Fournier <afournier@wazo.io>
2019-05-17 13:50:02 -0400

    wazo-gigaset: use long version format for N510 and N720

--- plugins/wazo-gigaset/N720/entry.py
+++ plugins/wazo-gigaset/N720/entry.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 common = {}
@@ -7,8 +7,8 @@ execfile_('common.py', common)
 
 
 MODEL_VERSIONS = {
-    u'N720 DM PRO': u'70.111',
-    u'N720 IP PRO': u'70.111',
+    u'N720 DM PRO': u'70.111.00.000.000',
+    u'N720 IP PRO': u'70.111.00.000.000',
 }
 
 
