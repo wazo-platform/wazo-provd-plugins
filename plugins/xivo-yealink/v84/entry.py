fd6b15c6a90a3f976fa26a41b608ce16f73a5a72
Alexandre Fournier <afournier@wazo.io>
2019-10-30 14:20:47 -0400

    wazo-yealink-v84: update firmwares

--- plugins/xivo-yealink/v84/entry.py
+++ plugins/xivo-yealink/v84/entry.py
@@ -1,48 +1,48 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2013-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2013-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 common_globals = {}
 execfile_('common.py', common_globals)
 
 MODEL_VERSIONS = {
-    u'CP920': u'78.84.0.15',
-    u'T19P_E2': u'53.84.0.15',
-    u'T21P_E2': u'52.84.0.15',
-    u'T23P': u'44.84.0.15',
-    u'T23G': u'44.84.0.15',
-    u'T27G': u'69.84.0.15',
-    u'T40P': u'54.84.0.15',
-    u'T40G': u'76.84.0.15',
-    u'T41S': u'66.84.0.15',
-    u'T42S': u'66.84.0.15',
-    u'T46S': u'66.84.0.15',
-    u'T48S': u'66.84.0.15',
-    u'T52S': u'70.84.0.15',
-    u'T53': u'95.84.0.30',
-    u'T53W': u'95.84.0.30',
-    u'T54S': u'70.84.0.15',
-    u'T54W': u'96.84.0.30',
-    u'T57W': u'97.84.0.30',
+    u'CP920': u'78.84.0.90',
+    u'T19P_E2': u'53.84.0.15',  # 53.84.0.90 version does not support YDMP and YMCS
+    u'T21P_E2': u'52.84.0.15',  # 52.84.0.90 version does not support YDMP and YMCS
+    u'T23P': u'44.84.0.15',  # 44.84.0.90 version does not support YDMP and YMCS
+    u'T23G': u'44.84.0.15',  # 44.84.0.90 version does not support YDMP and YMCS
+    u'T27G': u'69.84.0.90',
+    u'T40P': u'54.84.0.15',  # 54.84.0.90 version does not support YDMP and YMCS
+    u'T40G': u'76.84.0.15',  # 76.84.0.90 version does not support YDMP and YMCS
+    u'T41S': u'66.84.0.90',
+    u'T42S': u'66.84.0.90',
+    u'T46S': u'66.84.0.90',
+    u'T48S': u'66.84.0.90',
+    u'T52S': u'70.84.0.70',
+    u'T53': u'95.84.0.95',
+    u'T53W': u'95.84.0.95',
+    u'T54S': u'70.84.0.70',
+    u'T54W': u'96.84.0.95',
+    u'T57W': u'97.84.0.95',
 }
 
 COMMON_FILES = [
     ('y000000000044.cfg', u'T23-44.84.0.15.rom', 'model.tpl'),
-    ('y000000000069.cfg', u'T27G-69.84.0.15.rom', 'model.tpl'),
+    ('y000000000069.cfg', u'T27G-69.84.0.90.rom', 'model.tpl'),
     ('y000000000052.cfg', u'T21P_E2-52.84.0.15.rom', 'model.tpl'),
     ('y000000000053.cfg', u'T19P_E2-53.84.0.15.rom', 'model.tpl'),
     ('y000000000054.cfg', u'T40-54.84.0.15.rom', 'model.tpl'),
     ('y000000000076.cfg', u'T40G-76.84.0.15.rom', 'model.tpl'),
-    ('y000000000065.cfg', u'T46S(T48S,T42S,T41S)-66.84.0.15.rom', 'model.tpl'),
-    ('y000000000066.cfg', u'T46S(T48S,T42S,T41S)-66.84.0.15.rom', 'model.tpl'),
-    ('y000000000067.cfg', u'T46S(T48S,T42S,T41S)-66.84.0.15.rom', 'model.tpl'),
-    ('y000000000068.cfg', u'T46S(T48S,T42S,T41S)-66.84.0.15.rom', 'model.tpl'),
-    ('y000000000095.cfg', u'T53W(T53)-95.84.0.30.rom', 'model.tpl'),
-    ('y000000000070.cfg', u'T54S(T52S)-70.84.0.15.rom', 'model.tpl'),
-    ('y000000000096.cfg', u'T54W-96.84.0.30.rom', 'model.tpl'),
-    ('y000000000097.cfg', u'T57W-97.84.0.30.rom', 'model.tpl'),
-    ('y000000000078.cfg', u'CP920-78.84.0.15.rom', 'model.tpl'),
+    ('y000000000065.cfg', u'T46S(T48S,T42S,T41S)-66.84.0.90.rom', 'model.tpl'),
+    ('y000000000066.cfg', u'T46S(T48S,T42S,T41S)-66.84.0.90.rom', 'model.tpl'),
+    ('y000000000067.cfg', u'T46S(T48S,T42S,T41S)-66.84.0.90.rom', 'model.tpl'),
+    ('y000000000068.cfg', u'T46S(T48S,T42S,T41S)-66.84.0.90.rom', 'model.tpl'),
+    ('y000000000095.cfg', u'T53W(T53)-95.84.0.95.rom', 'model.tpl'),
+    ('y000000000070.cfg', u'T54S(T52S)-70.84.0.70.rom', 'model.tpl'),
+    ('y000000000096.cfg', u'T54W-96.84.0.95.rom', 'model.tpl'),
+    ('y000000000097.cfg', u'T57W-97.84.0.95.rom', 'model.tpl'),
+    ('y000000000078.cfg', u'CP920-78.84.0.90.rom', 'model.tpl'),
 ]
 
 
