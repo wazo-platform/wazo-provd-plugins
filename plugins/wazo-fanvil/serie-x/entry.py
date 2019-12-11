bd07540ce5440ba63236830590faef24ae8883fe
Alexandre Fournier <afournier@wazo.io>
2019-12-11 08:08:31 -0500

    fanvil: update X4/G firmware version

--- plugins/wazo-fanvil/serie-x/entry.py
+++ plugins/wazo-fanvil/serie-x/entry.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2010-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2010-2019 The Wazo Authors  (see the AUTHORS file)
 # Contributor: Sébastien Le Moal <sebastien.calexium.com>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -34,7 +34,7 @@ COMMON_FILES = [
     ('F0V00X200000.cfg', u'x2R2.2.0.3753T20170803142949.z', 'model.tpl'),
     ('F0V00X300000.cfg', u'x31.4.0.2016T20170303152729.z', 'model.tpl'),
     ('f0X3shw1.100.cfg', u'x3s2.4.0.5487T20180309151655.z', 'model.tpl'),
-    ('f0X4hw1.100.cfg', u'x42.4.0.5487T20180309151511.z', 'model.tpl'),
+    ('f0X4hw1.100.cfg', u'x42.10.2.6887T20191122095252.z', 'model.tpl'),
     ('f0X5hw1.100.cfg', u'x51.4.0.2016T20170303151233.z', 'model.tpl'),
     ('F0V00X5S0000.cfg', u'x5s-6900-P0.11.7-1.4.1-2303T2018-03-14-16.01.18.z', 'model.tpl'),
     ('F0V00X600000.cfg', u'x6-6904-P0.11.7-1.4.1-2303T2018-03-14-15.21.29.z', 'model.tpl'),
