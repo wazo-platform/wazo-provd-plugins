167abdd5869cf6964293c5654c0ee35d135f6b71
Yannick MOLINET <yannick@adn-systemes.fr>
2020-06-11 09:34:03 +0200

    Fix SPDX-License-Identifier and copyright

--- plugins/wazo-snom/10.1.54.13/entry.py
+++ plugins/wazo-snom/10.1.54.13/entry.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018 The Wazo Authors  (see AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0+
+# Copyright 2018-2020 The Wazo Authors (see AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 common_globals = {}
 execfile_('common.py', common_globals)
