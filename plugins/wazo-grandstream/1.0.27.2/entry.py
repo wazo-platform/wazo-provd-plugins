9f03c613dd13bf224cc95c647808ba283f4d56f2
Alexandre Fournier <afournier@wazo.io>
2021-06-11 15:28:18 -0400

    grandstream: add HT801/HT802 support

---
+++ plugins/wazo-grandstream/1.0.27.2/entry.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2021 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+common = {}
+execfile_('common.py', common)
+
+MODELS = [
+    u'HT801',
+    u'HT802',
+]
+VERSION = u'1.0.27.2'
+
+
+class GrandstreamPlugin(common['BaseGrandstreamPlugin']):
+    IS_PLUGIN = True
+
+    _MODELS = MODELS
+
+    pg_associator = common['BaseGrandstreamPgAssociator'](MODELS, VERSION)
