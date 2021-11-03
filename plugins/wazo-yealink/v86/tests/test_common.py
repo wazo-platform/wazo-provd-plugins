2915cbb4437e1fbf9b5a74d98bf48061db472a6d
Alexandre Fournier <afournier@wazo.io>
2021-11-03 15:58:09 -0400

    yealink-v86: refactor tests

--- plugins/wazo-yealink/v86/tests/test_common.py
+++ plugins/wazo-yealink/v86/tests/test_common.py
@@ -23,7 +23,10 @@ from ..common import BaseYealinkHTTPDeviceInfoExtractor, BaseYealinkPgAssociator
 from ..models import MODEL_VERSIONS
 
 
-class TestCommon(unittest.TestCase):
+class TestInfoExtraction(unittest.TestCase):
+    def setUp(self):
+        self.http_info_extractor = BaseYealinkHTTPDeviceInfoExtractor()
+
     def _mock_request(self, ua=None, path=None):
         request = MagicMock()
         request.getHeader = MagicMock(return_value=ua)
@@ -45,10 +48,10 @@ class TestCommon(unittest.TestCase):
                 'mac': '80:5e:c0:d5:7d:72',
             },
         }
-        http_info_extractor = BaseYealinkHTTPDeviceInfoExtractor()
+
         for ua, info in ua_infos.items():
             assert_that(
-                http_info_extractor._do_extract(self._mock_request(ua=ua)),
+                self.http_info_extractor._do_extract(self._mock_request(ua=ua)),
                 has_entries(info),
             )
 
@@ -61,47 +64,45 @@ class TestCommon(unittest.TestCase):
             '/249ad8123456.cfg': '24:9a:d8:12:34:56',
         }
 
-        http_info_extractor = BaseYealinkHTTPDeviceInfoExtractor()
-
         for path, mac in six.iteritems(macs_from_paths):
             assert_that(
-                http_info_extractor._do_extract(self._mock_request(path=path)),
+                self.http_info_extractor._do_extract(self._mock_request(path=path)),
                 has_entries({u'vendor': u'Yealink', u'mac': mac}),
             )
 
+
+class TestPluginAssociation(unittest.TestCase):
+    def setUp(self):
+        self.plugin_associator = BaseYealinkPgAssociator(MODEL_VERSIONS)
+
     def test_plugin_association_when_all_info_match(self):
-        plugin_associator = BaseYealinkPgAssociator(MODEL_VERSIONS)
         for model, version in six.iteritems(MODEL_VERSIONS):
             assert_that(
-                plugin_associator._do_associate('Yealink', model, version),
+                self.plugin_associator._do_associate('Yealink', model, version),
                 equal_to(FULL_SUPPORT),
             )
 
     def test_plugin_association_when_only_vendor_and_model_match(self):
-        plugin_associator = BaseYealinkPgAssociator(MODEL_VERSIONS)
         for model in six.iterkeys(MODEL_VERSIONS):
             assert_that(
-                plugin_associator._do_associate('Yealink', model, None),
+                self.plugin_associator._do_associate('Yealink', model, None),
                 equal_to(COMPLETE_SUPPORT),
             )
 
     def test_plugin_association_when_only_vendor_matches(self):
-        plugin_associator = BaseYealinkPgAssociator(MODEL_VERSIONS)
         assert_that(
-            plugin_associator._do_associate('Yealink', None, None),
+            self.plugin_associator._do_associate('Yealink', None, None),
             equal_to(PROBABLE_SUPPORT),
         )
 
     def test_plugin_association_when_nothing_matches(self):
-        plugin_associator = BaseYealinkPgAssociator(MODEL_VERSIONS)
         assert_that(
-            plugin_associator._do_associate('DoesNotMatch', 'NothingPhone', '1.2.3'),
+            self.plugin_associator._do_associate('DoesNotMatch', 'NothingPhone', '1.2.3'),
             equal_to(IMPROBABLE_SUPPORT),
         )
 
     def test_plugin_association_does_not_match_when_empty_strings(self):
-        plugin_associator = BaseYealinkPgAssociator(MODEL_VERSIONS)
         assert_that(
-            plugin_associator._do_associate('', '', ''),
+            self.plugin_associator._do_associate('', '', ''),
             equal_to(IMPROBABLE_SUPPORT),
         )
