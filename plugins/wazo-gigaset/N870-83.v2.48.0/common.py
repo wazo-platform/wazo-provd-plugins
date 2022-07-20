d1e4afbb71d5b4507664cb760bca2e9b2101b68d
BnLG <106586925+BnLG@users.noreply.github.com>
2022-07-20 20:39:52 +0200

    Gigaset N870 copyright update

--- plugins/wazo-gigaset/N870-83.v2.48.0/common.py
+++ plugins/wazo-gigaset/N870-83.v2.48.0/common.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2011-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2011-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 """Common code shared by the various wazo-gigaset plugins."""
--- plugins/wazo-gigaset/N870-83.v2.48.0/entry.py
+++ plugins/wazo-gigaset/N870-83.v2.48.0/entry.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 common = {}
