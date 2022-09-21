a2f856582df4d9bf86698a7ab3602d31fb08e380
Jesse Sopel <jesse.sopel@wazo.io>
2022-09-21 08:38:48 -0400

    Add tests for locales, timezones, funckeys and sip_lines, mac addresses Also ignore test coverage for entry.py

--- plugins/wazo-yealink/v86/tests/test_common.py
+++ plugins/wazo-yealink/v86/tests/test_common.py
@@ -6,6 +6,14 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = OSError
+
+from textwrap import dedent
+
+import pytest
 import six
 
 from hamcrest import (
@@ -15,6 +23,7 @@ from hamcrest import (
     has_properties,
 )
 from mock import MagicMock, patch, sentinel
+from provd.devices.config import RawConfigError
 from provd.devices.pgasso import (
     COMPLETE_SUPPORT,
     FULL_SUPPORT,
@@ -75,6 +84,25 @@ class TestInfoExtraction(object):
                 self.http_info_extractor._do_extract(self._mock_request(path=path)),
                 has_entries({u'vendor': u'Yealink', u'mac': mac}),
             )
+
+    @patch('v86.common.logger')
+    def test_invalid_mac(self, mocked_logger):
+        self.http_info_extractor._extract_from_ua('Yealink SIP-T20P 9.72.0.30 00:15:6525ef16a7c')
+        message, mac, exception = mocked_logger.warning.call_args.args
+        assert message == 'Could not normalize MAC address "%s": %s'
+        assert mac == '00:15:6525ef16a7c'
+        assert isinstance(exception, ValueError)
+        assert str(exception) == 'invalid MAC string'
+
+        mocked_logger.reset_mock()
+        request = MagicMock()
+        request.path = '/e434d7jabd'
+        self.http_info_extractor._extract_from_path(request)
+        message, mac, exception = mocked_logger.warning.call_args.args
+        assert message == 'Could not normalize MAC address "%s": %s'
+        assert mac == 'e434d7'
+        assert isinstance(exception, ValueError)
+        assert str(exception) == 'invalid MAC string'
 
 
 class TestPluginAssociation(object):
@@ -147,18 +175,165 @@ class TestPlugin(object):
             'http_port': '80',
             'locale': 'en_US',
             'funckeys': {},
-            'sip_lines': {},
+            'sip_proxy_ip': '1.1.1.1',
+            'sip_lines': {'1': {'number': '5888'}}
         }
         v86_plugin._tpl_helper.get_dev_template.return_value = 'template'
         v86_plugin.configure(device, raw_config)
-        assert raw_config['XX_country'] == 'United States'
-        assert raw_config['XX_lang'] == 'English'
-        assert raw_config['XX_handset_lang'] == '0'
         v86_plugin._tpl_helper.get_dev_template.assert_called_with('805ec0d57d72.cfg', device)
         v86_plugin._tpl_helper.dump.assert_called_with(
             'template', raw_config, 'test_dir/var/tftpboot/805ec0d57d72.cfg', 'UTF-8'
         )
 
+    @patch('os.remove')
+    def test_deconfigure(self, mocked_remove, v86_plugin):
+        v86_plugin.deconfigure({'mac': '80:5e:c0:d5:7d:72'})
+        mocked_remove.assert_called_with('test_dir/var/tftpboot/805ec0d57d72.cfg')
+
+    @patch('v86.common.logger')
+    def test_deconfigure_no_file(self, mocked_logger, v86_plugin):
+        v86_plugin.deconfigure({'mac': '00:00:00:00:00:00'})
+        message, exception = mocked_logger.info.call_args.args
+        assert message == 'error while removing file: %s'
+        assert isinstance(exception, FileNotFoundError)
+
     def test_sensitive_file(self, v86_plugin):
         assert v86_plugin.is_sensitive_filename('patate') is False
         assert v86_plugin.is_sensitive_filename('805ec0d57d72.cfg') is True
+
+    def test_add_country_and_lang(self, v86_plugin):
+        raw_config = {'locale': 'en_US'}
+        v86_plugin._add_country_and_lang(raw_config)
+        assert raw_config['XX_country'] == 'United States'
+        assert raw_config['XX_lang'] == 'English'
+        assert raw_config['XX_handset_lang'] == '0'
+        raw_config = {'locale': 'ja_JP'}
+        v86_plugin._add_country_and_lang(raw_config)
+        assert raw_config == {'locale': 'ja_JP'}
+
+    def test_checks(self, v86_plugin):
+        with pytest.raises(RawConfigError):
+            v86_plugin._check_config({})
+
+        with pytest.raises(Exception):
+            v86_plugin._check_device({})
+
+    @patch('v86.common.plugins')
+    def test_phonebook_url(self, provd_plugins, v86_plugin):
+        raw_config = {
+            'config_version': 0,
+            'X_xivo_phonebook_ip': '1.1.1.1',
+        }
+        v86_plugin._add_xivo_phonebook_url(raw_config)
+        provd_plugins.add_xivo_phonebook_url.assert_not_called()
+        expected_url = 'http://1.1.1.1/service/ipbx/web_services.php/phonebook/search/?name=#SEARCH'
+        assert raw_config['XX_xivo_phonebook_url'] == expected_url
+
+        raw_config = {'config_version': 1}
+        v86_plugin._add_xivo_phonebook_url(raw_config)
+        provd_plugins.add_xivo_phonebook_url.assert_called_with(
+            raw_config, 'yealink', entry_point='lookup', qs_suffix='term=#SEARCH',
+        )
+
+    def test_sip_lines(self, v86_plugin):
+        raw_config = {
+            'sip_dtmf_mode': 'SIP-INFO',
+            'exten_voicemail': '*98',
+            'sip_proxy_ip': '1.1.1.1',
+            'sip_proxy_port': '5080',
+            'XX_templates': {('1.1.1.1', '5080'): {'id': 5}},
+            'sip_lines': {
+                '1': {
+                    'number': '5888',
+                }
+            }
+        }
+        v86_plugin._add_xx_sip_lines({'model': 'patate'}, raw_config)
+        assert raw_config['sip_lines'] == raw_config['XX_sip_lines']
+
+        v86_plugin._update_sip_lines(raw_config)
+        v86_plugin._add_xx_sip_lines({'model': 'T33G'}, raw_config)
+        assert raw_config['XX_sip_lines'] == {
+            '1': {
+                'number': '5888',
+                'XX_line_no': 1,
+                'XX_dtmf_type': '2',
+                'voicemail': '*98',
+                'proxy_ip': '1.1.1.1',
+                'proxy_port': '5080',
+                'XX_template_id': 5
+            },
+            '2': None,
+            '3': None,
+            '4': None,
+        }
+
+    def test_timezones(self, v86_plugin):
+        raw_config = {'timezone': 'America/Montreal'}
+        v86_plugin._add_timezone(raw_config)
+        assert raw_config['XX_timezone'] == dedent(
+            """\
+            local_time.time_zone = -5
+            local_time.summer_time = 1
+            local_time.dst_time_type = 1
+            local_time.start_time = 3/2/7/2
+            local_time.end_time = 11/1/7/2
+            local_time.offset_time = 60"""
+        )
+        raw_config = {'timezone': 'America/Regina'}
+        v86_plugin._add_timezone(raw_config)
+        assert raw_config['XX_timezone'] == dedent(
+            """\
+            local_time.time_zone = -6
+            local_time.summer_time = 0"""
+        )
+
+    @patch('v86.common.logger')
+    def test_function_keys(self, mocked_logger, v86_plugin):
+        raw_config = {
+            'funckeys': {
+                '1': {
+                    'type': 'speeddial',
+                    'value': 'test_speed_dial',
+                    'label': 'Test Speed dial',
+                },
+                '4': {
+                    'type': 'other',
+                    'value': 'test',
+                },
+                '2': {
+                    'type': 'blf',
+                    'value': 'test_blf',
+                    'label': 'Test blf',
+                },
+                '3': {
+                    'type': 'park',
+                    'value': 'test_park',
+                    'label': 'Test Park',
+                },
+            },
+            'sip_lines': {
+                '1': {'number': '5888'}
+            }
+        }
+        v86_plugin._add_fkeys({'model': 'T33G'}, raw_config)
+        mocked_logger.info.assert_called_with('Unsupported funckey type: %s', 'other')
+        assert raw_config['XX_fkeys'] == dedent(
+            """\
+            linekey.1.type = 13
+            linekey.1.line = 1
+            linekey.1.value = test_speed_dial
+            linekey.1.label = Test Speed dial
+           
+            linekey.2.type = 16
+            linekey.2.line = 1
+            linekey.2.value = test_blf
+            linekey.2.label = Test blf
+            
+            linekey.3.type = 10
+            linekey.3.line = 1
+            linekey.3.value = test_park
+            linekey.3.label = Test Park
+            
+            """
+        )
