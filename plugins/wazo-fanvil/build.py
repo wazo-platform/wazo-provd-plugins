e5c857571a38b2a1f8ef964746fb0be333c7b89f
Franck <franckisys@gmail.com>
2023-03-27 13:41:33 +0000

    copyright

--- plugins/wazo-fanvil/build.py
+++ plugins/wazo-fanvil/build.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013-2022 The Wazo Authors  (see the AUTHORS file)
+Copyright 2013-2023 The Wazo Authors  (see the AUTHORS file)
 SPDX-License-Identifier: GPL-3.0-or-later
 
 Depends on the following external programs:
