d129d8229b6db84ea17621a724a8570606443be0
Alexandre Fournier <afournier@wazo.io>
2023-07-18 15:22:53 -0400

    snom-10.1.152.12: update copyright date

--- plugins/wazo-snom/10.1.152.12/entry.py
+++ plugins/wazo-snom/10.1.152.12/entry.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2022 The Wazo Authors (see AUTHORS file)
+# Copyright 2018-2023 The Wazo Authors (see AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 common_globals = {}
