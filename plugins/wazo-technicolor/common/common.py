88e87782f7a8093c444d5a75ec66f41a63dce552
Alexandre Fournier <afournier@wazo.io>
2023-09-08 16:58:42 -0400

    technicolor: use http_base_url

--- plugins/wazo-technicolor/common/common.py
+++ plugins/wazo-technicolor/common/common.py
@@ -1,17 +1,5 @@
 # Copyright 2011-2023 The Wazo Authors  (see the AUTHORS file)
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>
+# SPDX-License-Identifier: GPL-3.0-or-later
 from __future__ import annotations
 
 import logging
@@ -306,6 +294,16 @@ class BaseTechnicolorPlugin(StandardPlugin):
             raw_config, 'thomson', entry_point='lookup', qs_suffix='term=#SEARCH'
         )
 
+    def _add_server_url(self, raw_config):
+        if 'http_base_url' in raw_config:
+            _, _, remaining_url = raw_config['http_base_url'].partition('://')
+            raw_config['XX_server_url'] = raw_config['http_base_url']
+            raw_config['XX_server_url_without_scheme'] = remaining_url
+        else:
+            base_url = f"{raw_config['ip']}:{raw_config['http_port']}"
+            raw_config['XX_server_url_without_scheme'] = base_url
+            raw_config['XX_server_url'] = f"http://{base_url}"
+
     def _dev_specific_filename(self, device: dict[str, str]) -> str:
         # Return the device specific filename (not pathname) of device
         formatted_mac = format_mac(device['mac'], separator='', uppercase=True)
@@ -332,6 +330,7 @@ class BaseTechnicolorPlugin(StandardPlugin):
         self._add_ntp_zone_num(raw_config)
         self._add_fkeys(raw_config)
         self._add_xivo_phonebook_url(raw_config)
+        self._add_server_url(raw_config)
         raw_config['XX_phonebook_name'] = self._gen_xx_phonebook_name(raw_config)
         raw_config['XX_nb_lines'] = self._NB_LINES
 
