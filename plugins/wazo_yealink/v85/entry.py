890e6808f439c8dbd3808f612f9a3f822967e7e4
Alexandre Fournier <afournier@wazo.io>
2023-12-12 12:57:03 -0500

    yealink-v85: remove common configuration files and move everything into MAC-based file
    Why: when using a provisioning key, it is not possible to configure a common file with URLs in them. The file is common
    to all the devices and is generated at wazo-provd startup. This means that there is no provisioning key available.

--- plugins/wazo_yealink/v85/entry.py
+++ plugins/wazo_yealink/v85/entry.py
@@ -4,56 +4,6 @@
 common_globals = {}
 execfile_('common.py', common_globals)
 
-MODEL_VERSIONS = {
-    'CP960': '73.85.0.5',
-    'CP920': '78.85.0.5',
-    'T27G': '69.85.0.5',
-    'T30': '124.85.0.40',
-    'T30P': '124.85.0.40',
-    'T31': '124.85.0.40',
-    'T31P': '124.85.0.40',
-    'T31G': '124.85.0.40',
-    'T33P': '124.85.0.40',
-    'T33G': '124.85.0.40',
-    'T41S': '66.85.0.5',
-    'T42S': '66.85.0.5',
-    'T46S': '66.85.0.5',
-    'T48S': '66.85.0.5',
-    'T53': '96.85.0.5',
-    'T53W': '96.85.0.5',
-    'T54W': '96.85.0.5',
-    'T57W': '96.85.0.5',
-    'T58': '58.85.0.5',
-    'W60B': '77.85.0.25',
-    'W70B': '146.85.0.37',
-    'W90DM': '130.85.0.44',
-    'W90B': '130.85.0.44',
-}
-
-COMMON_FILES = [
-    ('y000000000069.cfg', 'T27G-69.85.0.5.rom', 'model.tpl'),
-    ('y000000000065.cfg', 'T46S(T48S,T42S,T41S)-66.85.0.5.rom', 'model.tpl'),
-    ('y000000000066.cfg', 'T46S(T48S,T42S,T41S)-66.85.0.5.rom', 'model.tpl'),
-    ('y000000000067.cfg', 'T46S(T48S,T42S,T41S)-66.85.0.5.rom', 'model.tpl'),
-    ('y000000000068.cfg', 'T46S(T48S,T42S,T41S)-66.85.0.5.rom', 'model.tpl'),
-    ('y000000000073.cfg', 'CP960-73.85.0.5.rom', 'model.tpl'),
-    ('y000000000078.cfg', 'CP920-78.85.0.5.rom', 'model.tpl'),
-    (
-        'y000000000123.cfg',
-        'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
-        'model.tpl',
-    ),
-    (
-        'y000000000124.cfg',
-        'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
-        'model.tpl',
-    ),
-    (
-        'y000000000127.cfg',
-        'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
-        'model.tpl',
-    ),
-]
 
 HANDSETS_FW = {
     'w53h': 'W53H-88.85.0.20.rom',
@@ -65,58 +15,116 @@ HANDSETS_FW = {
     't54w_dd10k': 'T54W-ddphone-96.85.0.65.rom ',
 }
 
-COMMON_FILES_DECT = [
-    {
-        'filename': 'y000000000130.cfg',
-        'fw_filename': '$PN-130.85.0.44.rom',  # $PN = Product Name, i.e W90B/W90DM
-        'handsets_fw': HANDSETS_FW,
-        'tpl_filename': 'dect_model.tpl',
+MODEL_INFO = {
+    'CP960': {
+        'version': '73.85.0.5',
+        'firmware': 'CP960-73.85.0.5.rom',
     },
-    {
-        'filename': 'y000000000058.cfg',
-        'fw_filename': 'T58-58.85.0.5.rom',
-        'handsets_fw': HANDSETS_FW,
-        'tpl_filename': 'dect_model.tpl',
+    'CP920': {
+        'version': '78.85.0.5',
+        'firmware': 'CP920-78.85.0.5.rom',
     },
-    {
-        'filename': 'y000000000077.cfg',
-        'fw_filename': 'W60B-77.85.0.25.rom',
-        'handsets_fw': HANDSETS_FW,
-        'tpl_filename': 'dect_model.tpl',
+    'T27G': {
+        'version': '69.85.0.5',
+        'firmware': 'T27G-69.85.0.5.rom',
     },
-    {
-        'filename': 'y000000000146.cfg',
-        'fw_filename': 'W70B-146.85.0.37.rom',
-        'handsets_fw': HANDSETS_FW,
-        'tpl_filename': 'dect_model.tpl',
+    'T30': {
+        'version': '124.85.0.40',
+        'firmware': 'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
     },
-    {
-        'filename': 'y000000000095.cfg',
-        'fw_filename': 'T54W(T57W,T53W,T53)-96.85.0.5.rom',
-        'handsets_fw': HANDSETS_FW,
-        'tpl_filename': 'dect_model.tpl',
+    'T30P': {
+        'version': '124.85.0.40',
+        'firmware': 'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
     },
-    {
-        'filename': 'y000000000096.cfg',
-        'fw_filename': 'T54W(T57W,T53W,T53)-96.85.0.5.rom',
-        'handsets_fw': HANDSETS_FW,
-        'tpl_filename': 'dect_model.tpl',
+    'T31': {
+        'version': '124.85.0.40',
+        'firmware': 'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
     },
-    {
-        'filename': 'y000000000097.cfg',
-        'fw_filename': 'T54W(T57W,T53W,T53)-96.85.0.5.rom',
-        'handsets_fw': HANDSETS_FW,
-        'tpl_filename': 'dect_model.tpl',
+    'T31P': {
+        'version': '124.85.0.40',
+        'firmware': 'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
     },
-]
+    'T31G': {
+        'version': '124.85.0.40',
+        'firmware': 'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
+    },
+    'T33P': {
+        'version': '124.85.0.40',
+        'firmware': 'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
+    },
+    'T33G': {
+        'version': '124.85.0.40',
+        'firmware': 'T31(T30,T30P,T31G,T31P,T33P,T33G)-124.85.0.40.rom',
+    },
+    'T41S': {
+        'version': '66.85.0.5',
+        'firmware': 'T46S(T48S,T42S,T41S)-66.85.0.5.rom',
+    },
+    'T42S': {
+        'version': '66.85.0.5',
+        'firmware': 'T46S(T48S,T42S,T41S)-66.85.0.5.rom',
+    },
+    'T46S': {
+        'version': '66.85.0.5',
+        'firmware': 'T46S(T48S,T42S,T41S)-66.85.0.5.rom',
+    },
+    'T48S': {
+        'version': '66.85.0.5',
+        'firmware': 'T46S(T48S,T42S,T41S)-66.85.0.5.rom',
+    },
+    'T53': {
+        'version': '96.85.0.5',
+        'firmware': 'T54W(T57W,T53W,T53)-96.85.0.5.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'T53W': {
+        'version': '96.85.0.5',
+        'firmware': 'T54W(T57W,T53W,T53)-96.85.0.5.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'T54W': {
+        'version': '96.85.0.5',
+        'firmware': 'T54W(T57W,T53W,T53)-96.85.0.5.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'T57W': {
+        'version': '96.85.0.5',
+        'firmware': 'T54W(T57W,T53W,T53)-96.85.0.5.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'T58': {
+        'version': '58.85.0.5',
+        'firmware': 'T58-58.85.0.5.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W60B': {
+        'version': '77.85.0.25',
+        'firmware': 'W60B-77.85.0.25.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W70B': {
+        'version': '146.85.0.37',
+        'firmware': 'W70B-146.85.0.37.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W90DM': {
+        'version': '130.85.0.44',
+        'firmware': '$PN-130.85.0.44.rom',  # $PN = Product Name, i.e W90DM
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W90B': {
+        'version': '130.85.0.44',
+        'firmware': '$PN-130.85.0.44.rom',  # $PN = Product Name, i.e W90B
+        'handsets_fw': HANDSETS_FW,
+    },
+}
 
 
 class YealinkPlugin(common_globals['BaseYealinkPlugin']):
     IS_PLUGIN = True
 
-    pg_associator = common_globals['BaseYealinkPgAssociator'](MODEL_VERSIONS)
+    pg_associator = common_globals['BaseYealinkPgAssociator'](MODEL_INFO)
 
     # Yealink plugin specific stuff
 
-    _COMMON_FILES = COMMON_FILES
-    _COMMON_FILES_DECT = COMMON_FILES_DECT
+    _MODEL_INFO = MODEL_INFO
--- plugins/wazo_yealink/v85/templates/common/model.tpl
+++
@@ -1,4 +0,0 @@
-#!version:1.0.0.1
-
-static.auto_provision.server.url = {{ XX_server_url }}/
-static.firmware.url = {{ XX_server_url }}/firmware/{{ XX_fw_filename }}
