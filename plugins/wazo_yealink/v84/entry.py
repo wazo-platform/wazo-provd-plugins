be9ff85c893ca0b5d784e4f3a06b0adf23b2dd02
Alexandre Fournier <afournier@wazo.io>
2023-12-13 09:41:13 -0500

    yealink-v84: remove common configuration files

--- plugins/wazo_yealink/v84/entry.py
+++ plugins/wazo_yealink/v84/entry.py
@@ -1,58 +1,98 @@
-# Copyright 2013-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2013-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 common_globals = {}
 execfile_('common.py', common_globals)
 
-MODEL_VERSIONS = {
-    'CP920': '78.84.0.125',
-    'CP960': '73.84.0.25',
-    'T19P_E2': '53.84.0.125',  # >=53.84.0.90 version does not support YDMP and YMCS
-    'T21P_E2': '52.84.0.125',  # >=52.84.0.90 version does not support YDMP and YMCS
-    'T23P': '44.84.0.140',  # >=44.84.0.90 version does not support YDMP and YMCS
-    'T23G': '44.84.0.140',  # >=44.84.0.90 version does not support YDMP and YMCS
-    'T27G': '69.84.0.125',
-    'T40P': '54.84.0.125',  # >=54.84.0.90 version does not support YDMP and YMCS
-    'T40G': '76.84.0.125',  # >=76.84.0.90 version does not support YDMP and YMCS
-    'T41S': '66.84.0.125',
-    'T42S': '66.84.0.125',
-    'T46S': '66.84.0.125',
-    'T48S': '66.84.0.125',
-    'T52S': '70.84.0.70',
-    'T53': '95.84.0.125',
-    'T53W': '95.84.0.125',
-    'T54S': '70.84.0.70',
-    'T54W': '96.84.0.125',
-    'T57W': '97.84.0.125',
-    'T58': '58.84.0.25',
+MODEL_INFO = {
+    'CP920': {
+        'version': '78.84.0.125',
+        'firmware': 'CP920-78.84.0.125.rom',
+    },
+    'CP960': {
+        'version': '73.84.0.25',
+        'firmware': 'CP960-73.84.0.25.rom',
+    },
+    'T19P_E2': {
+        'version': '53.84.0.125',  # >=53.84.0.90 version does not support YDMP and YMCS
+        'firmware': 'T19P_E2-53.84.0.125.rom',
+    },
+    'T21P_E2': {
+        'version': '52.84.0.125',  # >=52.84.0.90 version does not support YDMP and YMCS
+        'firmware': 'T21P_E2-52.84.0.125.rom',
+    },
+    'T23P': {
+        'version': '44.84.0.140',  # >=44.84.0.90 version does not support YDMP and YMCS
+        'firmware': 'T23-44.84.0.140.rom',
+    },
+    'T23G': {
+        'version': '44.84.0.140',  # >=44.84.0.90 version does not support YDMP and YMCS
+        'firmware': 'T23-44.84.0.140.rom',
+    },
+    'T27G': {
+        'version': '69.84.0.125',
+        'firmware': 'T27G-69.84.0.125.rom',
+    },
+    'T40P': {
+        'version': '54.84.0.125',  # >=54.84.0.90 version does not support YDMP and YMCS
+        'firmware': 'T40-54.84.0.125.rom',
+    },
+    'T40G': {
+        'version': '76.84.0.125',  # >=76.84.0.90 version does not support YDMP and YMCS
+        'firmware': 'T40G-76.84.0.125.rom',
+    },
+    'T41S': {
+        'version': '66.84.0.125',
+        'firmware': 'T46S(T48S,T42S,T41S)-66.84.0.125.rom',
+    },
+    'T42S': {
+        'version': '66.84.0.125',
+        'firmware': 'T46S(T48S,T42S,T41S)-66.84.0.125.rom',
+    },
+    'T46S': {
+        'version': '66.84.0.125',
+        'firmware': 'T46S(T48S,T42S,T41S)-66.84.0.125.rom',
+    },
+    'T48S': {
+        'version': '66.84.0.125',
+        'firmware': 'T46S(T48S,T42S,T41S)-66.84.0.125.rom',
+    },
+    'T52S': {
+        'version': '70.84.0.70',
+        'firmware': 'T54S(T52S)-70.84.0.70.rom',
+    },
+    'T53': {
+        'version': '95.84.0.125',
+        'firmware': 'T53W(T53)-95.84.0.125.rom',
+    },
+    'T53W': {
+        'version': '95.84.0.125',
+        'firmware': 'T53W(T53)-95.84.0.125.rom',
+    },
+    'T54S': {
+        'version': '70.84.0.70',
+        'firmware': 'T54S(T52S)-70.84.0.70.rom',
+    },
+    'T54W': {
+        'version': '96.84.0.125',
+        'firmware': 'T54W-96.84.0.125.rom',
+    },
+    'T57W': {
+        'version': '97.84.0.125',
+        'firmware': 'T57W-97.84.0.125.rom',
+    },
+    'T58': {
+        'version': '58.84.0.25',
+        'firmware': 'T58-58.84.0.25.rom',
+    },
 }
-
-COMMON_FILES = [
-    ('y000000000044.cfg', 'T23-44.84.0.140.rom', 'model.tpl'),
-    ('y000000000069.cfg', 'T27G-69.84.0.125.rom', 'model.tpl'),
-    ('y000000000052.cfg', 'T21P_E2-52.84.0.125.rom', 'model.tpl'),
-    ('y000000000053.cfg', 'T19P_E2-53.84.0.125.rom', 'model.tpl'),
-    ('y000000000054.cfg', 'T40-54.84.0.125.rom', 'model.tpl'),
-    ('y000000000076.cfg', 'T40G-76.84.0.125.rom', 'model.tpl'),
-    ('y000000000065.cfg', 'T46S(T48S,T42S,T41S)-66.84.0.125.rom', 'model.tpl'),
-    ('y000000000066.cfg', 'T46S(T48S,T42S,T41S)-66.84.0.125.rom', 'model.tpl'),
-    ('y000000000067.cfg', 'T46S(T48S,T42S,T41S)-66.84.0.125.rom', 'model.tpl'),
-    ('y000000000068.cfg', 'T46S(T48S,T42S,T41S)-66.84.0.125.rom', 'model.tpl'),
-    ('y000000000095.cfg', 'T53W(T53)-95.84.0.125.rom', 'model.tpl'),
-    ('y000000000070.cfg', 'T54S(T52S)-70.84.0.70.rom', 'model.tpl'),
-    ('y000000000096.cfg', 'T54W-96.84.0.125.rom', 'model.tpl'),
-    ('y000000000097.cfg', 'T57W-97.84.0.125.rom', 'model.tpl'),
-    ('y000000000058.cfg', 'T58-58.84.0.25.rom', 'model.tpl'),
-    ('y000000000078.cfg', 'CP920-78.84.0.125.rom', 'model.tpl'),
-    ('y000000000073.cfg', 'CP960-73.84.0.25.rom', 'model.tpl'),
-]
 
 
 class YealinkPlugin(common_globals['BaseYealinkPlugin']):
     IS_PLUGIN = True
 
-    pg_associator = common_globals['BaseYealinkPgAssociator'](MODEL_VERSIONS)
+    pg_associator = common_globals['BaseYealinkPgAssociator'](MODEL_INFO)
 
     # Yealink plugin specific stuff
 
-    _COMMON_FILES = COMMON_FILES
+    _MODEL_INFO = MODEL_INFO
--- plugins/wazo_yealink/v84/tests/test_common.py
+++ plugins/wazo_yealink/v84/tests/test_common.py
@@ -124,37 +124,37 @@ class TestInfoExtraction:
 
 class TestPluginAssociation:
     def test_plugin_association_when_all_info_match(self, v84_entry):
-        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_VERSIONS)
-        for model, version in v84_entry.MODEL_VERSIONS.items():
+        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_INFO)
+        for model, info in v84_entry.MODEL_INFO.items():
             assert (
-                plugin_associator._do_associate('Yealink', model, version)
+                plugin_associator._do_associate('Yealink', model, info['version'])
                 == DeviceSupport.EXACT
             )
 
     def test_plugin_association_when_only_vendor_and_model_match(self, v84_entry):
-        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_VERSIONS)
-        for model in v84_entry.MODEL_VERSIONS:
+        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_INFO)
+        for model in v84_entry.MODEL_INFO:
             assert (
                 plugin_associator._do_associate('Yealink', model, None)
                 == DeviceSupport.COMPLETE
             )
 
     def test_plugin_association_when_only_vendor_matches(self, v84_entry):
-        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_VERSIONS)
+        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_INFO)
         assert (
             plugin_associator._do_associate('Yealink', None, None)
             == DeviceSupport.PROBABLE
         )
 
     def test_plugin_association_when_nothing_matches(self, v84_entry):
-        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_VERSIONS)
+        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_INFO)
         assert (
             plugin_associator._do_associate('DoesNotMatch', 'NothingPhone', '1.2.3')
             == DeviceSupport.IMPROBABLE
         )
 
     def test_plugin_association_does_not_match_when_empty_strings(self, v84_entry):
-        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_VERSIONS)
+        plugin_associator = BaseYealinkPgAssociator(v84_entry.MODEL_INFO)
         assert plugin_associator._do_associate('', '', '') == DeviceSupport.IMPROBABLE
 
 
@@ -168,13 +168,6 @@ class TestPlugin:
         )
         assert plugin.services == sentinel.fetchfw_services
         fetch_fw.assert_called_once_with('test_dir', sentinel.fetchfw_downloaders)
-
-    def test_common_configure(self, v84_plugin):
-        raw_config = {'http_base_url': 'http://localhost:8667'}
-        v84_plugin._tpl_helper.get_template.return_value = 'template'
-        v84_plugin.configure_common(raw_config)
-        v84_plugin._tpl_helper.get_template.assert_called_with('common/model.tpl')
-        assert len(v84_plugin._tpl_helper.dump.mock_calls) == 17
 
     def test_configure(self, v84_plugin):
         device = {
