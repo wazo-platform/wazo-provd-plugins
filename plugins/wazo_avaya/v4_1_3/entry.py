8f8a3c1b2ce49a0705a6d99cf77047f49ddd93d4
fouille <franckisys@gmail.com>
2024-03-26 12:50:43 +0100

    Add J100 series (J129, J169 and 179)

---
+++ plugins/wazo_avaya/v4_1_3/entry.py
@@ -0,0 +1,30 @@
+# Copyright 2013-2023 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Plugin for Avaya J100 series using the 04.01.03.06 SIP software."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import TypedDict
+
+    from ..common.common import BaseAvayaPgAssociator, BaseAvayaPlugin  # noqa: F401
+
+    class CommonGlobalsDict(TypedDict):
+        BaseAvayaPlugin: type[BaseAvayaPlugin]
+        BaseAvayaPgAssociator: type[BaseAvayaPgAssociator]
+
+
+common_globals: CommonGlobalsDict = {}  # type: ignore[typeddict-item]
+execfile_('common.py', common_globals)  # type: ignore[name-defined]
+
+
+MODELS = ['J129', 'J169', 'J179']
+VERSION = '04.01.03.06'
+
+
+class AvayaPlugin(common_globals['BaseAvayaPlugin']):  # type: ignore[valid-type,misc]
+    IS_PLUGIN = True
+
+    pg_associator = common_globals['BaseAvayaPgAssociator'](MODELS, VERSION)
