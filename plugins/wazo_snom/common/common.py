1e336a9b36c9f2c280758ec5e01fd3aca44141a3
fouille <franckisys@gmail.com>
2024-09-27 15:24:31 +0200

    modified:   plugins/wazo_snom/common/common.py

--- plugins/wazo_snom/common/common.py
+++ plugins/wazo_snom/common/common.py
@@ -299,7 +299,9 @@ class BaseSnomPlugin(StandardPlugin):
     def _add_xivo_phonebook_url(self, raw_config):
         for model in self._MODELS:
             if model.startswith("D8"):
-                plugins.add_xivo_phonebook_url(raw_config, 'snom', entry_point='lookup', qs_suffix='term=#')
+                plugins.add_xivo_phonebook_url(
+                    raw_config, 'snom', entry_point='lookup', qs_suffix='term=#'
+                )
             else:
                 plugins.add_xivo_phonebook_url(raw_config, 'snom')
 
