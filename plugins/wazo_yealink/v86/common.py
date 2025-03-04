12b587f9a4cf2eb7d9459aeb0d6ab02aa502d4f8
fouille <franckisys@gmail.com>
2025-03-04 15:45:04 +0100

    final fixs & tests

--- plugins/wazo_yealink/v86/common.py
+++ plugins/wazo_yealink/v86/common.py
@@ -251,6 +251,7 @@ class BaseYealinkFunckeyPrefixIterator:
         'T57W': 29,
         'T58': 27,
         'T58W': 27,
+        'AX83H': 0,
     }
     _NB_MEMORYKEY = {
         'CP920': 0,
@@ -283,6 +284,7 @@ class BaseYealinkFunckeyPrefixIterator:
         'T57W': 0,
         'T58': 0,
         'T58W': 0,
+        'AX83H': 0,
     }
 
     class NullExpansionModule:
@@ -383,6 +385,7 @@ class BaseYealinkPlugin(StandardPlugin):
         'T57W': 16,
         'T58': 16,
         'T58W': 16,
+        'AX83H': 4,
     }
     _SENSITIVE_FILENAME_REGEX = re.compile(r'^[0-9a-f]{12}\.cfg')
 
