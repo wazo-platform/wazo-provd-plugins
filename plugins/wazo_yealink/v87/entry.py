4f759c9ebe81e6894fc6bc2872dbf65051d0cfbf
Quentin Leconte <quentin.leconte@acrib.fr>
2025-08-20 19:19:04 +0200

    Yealink v87 | Add DECT W70/W75/W80/W90 support

--- plugins/wazo_yealink/v87/entry.py
+++ plugins/wazo_yealink/v87/entry.py
@@ -20,9 +20,12 @@ execfile_('common.py', common_globals)  # type: ignore[name-defined]
 
 
 HANDSETS_FW = {
-    'w53h': 'W53H-88.85.0.20.rom',
-    'w56h': 'W56H-61.85.0.20.rom',
-    'w59r': 'W59R-115.85.0.20.rom',
+    'w56h': 'W56H-61.87.0.5.rom',
+    'w57r': 'W57R-118.87.0.5.rom',
+    'w59r': 'W59R-115.87.0.5.rom',
+    'w73h': 'W73H-116.87.0.5.rom',
+    'w74h': 'W74H-119.87.0.5.rom',
+    'w78h': 'W78H-16.87.0.5.rom',
     'cp930w': 'CP930W-87.85.0.20.rom',
     't41s_dd10k': 'T4S-ddphone-66.85.0.56.rom',
     't54w_dd10k': 'T54W-ddphone-96.85.0.65.rom ',
@@ -72,6 +75,41 @@ MODEL_INFO = {
     'AX86R': {
         'version': '180.87.0.5',
         'firmware': 'AX83(AX83,AX86)-180.87.0.5.rom',
+    },
+    'W70B': {
+        'version': '146.87.0.15',
+        'firmware': 'W70B-146.87.0.15.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W75DM': {
+        'version': '175.87.0.10',
+        'firmware': 'W75DM-175.87.0.10.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W75B': {
+        'version': '175.87.0.10',
+        'firmware': 'W75DM-175.87.0.10.rom',  # Yealink Inject W75B fw inside W75DM
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W80DM': {
+        'version': '103.87.0.10',
+        'firmware': '$PN-103.87.0.10.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W80B': {
+        'version': '103.87.0.10',
+        'firmware': '$PN-103.87.0.10.rom',
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W90DM': {
+        'version': '130.87.0.10',
+        'firmware': '$PN-130.87.0.10.rom',  # $PN = Product Name, i.e W90DM
+        'handsets_fw': HANDSETS_FW,
+    },
+    'W90B': {
+        'version': '130.87.0.10',
+        'firmware': '$PN-130.87.0.10.rom',  # $PN = Product Name, i.e W90B
+        'handsets_fw': HANDSETS_FW,
     },
 }
 
