b8b98f08c1f9a610c64e032f10a98145b9263c5b
BnLG <106586925+BnLG@users.noreply.github.com>
2025-12-22 12:08:22 +0100

    Snom, 10.1.215.13 upgrade

---
+++ plugins/wazo_snom/v10_1_215_13/entry.py
@@ -0,0 +1,46 @@
+# Copyright 2018-2025 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import TypedDict
+
+    from ..common.common import BaseSnomPgAssociator, BaseSnomPlugin  # noqa: F401
+
+    class CommonGlobalsDict(TypedDict):
+        BaseSnomPlugin: type[BaseSnomPlugin]
+        BaseSnomPgAssociator: type[BaseSnomPgAssociator]
+
+
+common_globals: CommonGlobalsDict = {}  # type: ignore[typeddict-item]
+execfile_('common.py', common_globals)  # type: ignore[name-defined]
+
+MODELS = [
+    'D140',
+    'D150',
+    'D335',
+    'D385',
+    'D713',
+    '715',
+    'D717',
+    'D735',
+    'D785',
+    'D810',
+    'D812',
+    'D815',
+    'D862',
+    'D865',
+    'D892',
+]
+VERSION = '10.1.215.13'
+
+
+class SnomPlugin(common_globals['BaseSnomPlugin']):  # type: ignore[valid-type,misc]
+    IS_PLUGIN = True
+
+    _MODELS = MODELS
+
+    pg_associator = common_globals['BaseSnomPgAssociator'](MODELS, VERSION)
