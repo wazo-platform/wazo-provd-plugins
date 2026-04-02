fdb68a584685e3be73bf006582b1775d56918d08
Franck M <franckisys@gmail.com>
2026-04-02 14:55:30 +0200

    🚀 Others new firmwares

--- plugins/wazo_yealink/v87/entry.py
+++ plugins/wazo_yealink/v87/entry.py
@@ -49,40 +49,40 @@ MODEL_INFO = {
         'firmware': 'T58W-OS12-150.87.0.15.rom',
     },
     'T73U': {
-        'version': '185.87.0.10',
-        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.10.rom',
+        'version': '185.87.0.33',
+        'firmware': 'T77(T73,T74,T73U,T74U,T77U,T85W,T87W)-185.87.0.33.rom',
     },
     'T73W': {
-        'version': '185.87.0.10',
-        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.10.rom',
+        'version': '185.87.0.33',
+        'firmware': 'T77(T73,T74,T73U,T74U,T77U,T85W,T87W)-185.87.0.33.rom',
     },
     'T74U': {
-        'version': '185.87.0.10',
-        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.10.rom',
+        'version': '185.87.0.33',
+        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.33.rom',
     },
     'T74W': {
-        'version': '185.87.0.10',
-        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.10.rom',
+        'version': '185.87.0.33',
+        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.33.rom',
     },
     'T77U': {
-        'version': '185.87.0.10',
-        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.10.rom',
+        'version': '185.87.0.33',
+        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.33.rom',
     },
     'T85W': {
-        'version': '185.87.0.10',
-        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.10.rom',
+        'version': '185.87.0.33',
+        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.33.rom',
     },
     'T87W': {
-        'version': '185.87.0.10',
-        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.10.rom',
+        'version': '185.87.0.33',
+        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.33.rom',
     },
     'T88W': {
-        'version': '185.87.0.10',
-        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.10.rom',
+        'version': '192.87.0.10',
+        'firmware': 'T88(T88W,T88V)-192.87.0.10.rom',
     },
     'T88V': {
-        'version': '185.87.0.10',
-        'firmware': 'T77(T73,T74,T73U,T74U,T77,T77U,T85W,T87W)-185.87.0.10.rom',
+        'version': '192.87.0.10',
+        'firmware': 'T88(T88W,T88V)-192.87.0.10.rom',
     },
     'AX83H': {
         'version': '180.87.0.15',
@@ -93,13 +93,13 @@ MODEL_INFO = {
         'firmware': 'AX83(AX83,AX86)-180.87.0.15.rom',
     },
     'W70B': {
-        'version': '146.87.0.15',
-        'firmware': 'W70B-146.87.0.15.rom',
+        'version': '146.87.0.21',
+        'firmware': 'W70B-146.87.0.21.rom',
         'handsets_fw': HANDSETS_FW,
     },
     'W75DM': {
-        'version': '175.87.0.10',
-        'firmware': 'W75DM-175.87.0.10.rom',
+        'version': '175.87.0.18',
+        'firmware': 'W75DM-175.87.0.18.rom',
         'handsets_fw': HANDSETS_FW,
     },
     'W75B': {
@@ -108,8 +108,8 @@ MODEL_INFO = {
         'handsets_fw': HANDSETS_FW,
     },
     'W80DM': {
-        'version': '103.87.0.10',
-        'firmware': '$PN-103.87.0.10.rom',
+        'version': '103.87.0.18',
+        'firmware': 'W80DM-103.87.0.18.rom',
         'handsets_fw': HANDSETS_FW,
     },
     'W80B': {
@@ -118,8 +118,8 @@ MODEL_INFO = {
         'handsets_fw': HANDSETS_FW,
     },
     'W90DM': {
-        'version': '130.87.0.10',
-        'firmware': '$PN-130.87.0.10.rom',  # $PN = Product Name, i.e W90DM
+        'version': '130.87.0.18',
+        'firmware': '$PN-130.87.0.18.rom',  # $PN = Product Name, i.e W90DM
         'handsets_fw': HANDSETS_FW,
     },
     'W90B': {
